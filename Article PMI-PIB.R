cf431b500e8ae1ca981f79e42f00d56194f43e2f
Paul Favier <paul.favier4@gmail.com>
2022-11-10 00:28:53 +0100

    maj 10/11

--- Article PMI-PIB.R
+++ Article PMI-PIB.R
@@ -28,15 +28,15 @@ library(utilities)
 # la traiter
 #et (ii) ne sachant pas comment faire pour considerer les dates en "dates"
 
-#J'ai decide de tout fusionner dans une page d'excel et de creer une nouvelle 
+# -> J'ai decide de tout fusionner dans une page d'excel et de creer une nouvelle 
 #variable "periodes" pour pouvoir traiter ces infos.
 
 #Tout ce travail a ete fait prealablement sur excel
 
 #/!\ REMIND : un "pays" Synth a ete creer dans la bd pour faciliter la synthetisation, 
-#il sera peut etre amene a disparait si pas utile
+#il sera peut etre amene a disparaitre inutile
 
-db <- read_excel("Base sans NZ v2.xlsx")
+db <- read_excel("~/M2 PP/Memoire M2/Memoire algo/Base sans NZ v2.xlsx")
 
 #Apres verification a la louche il semblerait que toutes les variables soient
 #numeriques
@@ -61,7 +61,7 @@ db <-as.data.frame(db)
 # PARTIE 1 #
 
 #La fonction dataprep() du package Synth permet de creer des sous-bases pour 
-#automatiser et faciliter la synthetisation 
+#automatiser et faciliter la synthetisation
 
 fra <- dataprep(foo = db,
              predictors = "composite" ,
@@ -82,9 +82,28 @@ fra <- dataprep(foo = db,
 #Axes d'ameliorations :
 
 # (i) chercher a faire tourner la fonction avec les dates et non des periodes
-# (ii) bien avoir en tete l'utilite et le fonctionnement de time.optimize.ssr
+#(ii) faire tourner avec d'autres variables predictors, pour constater la potentielle
+#amelioration de l'estimation (Hyp: elle sera forcemment plus precise car elle est 
+#au moins aussi precise que l'estimation qui considere seulement la variable composite)
 
 
+#Pi : 
+#"foo =" indique la base de donnees dans laquelle on travaille
+#"predictors =" permet d'indiquer a partir de quel(les) variable(s) on realisera la synth
+#"predictors.op=" permet d'indiquer la methode de synthetisation (moyenne par default)
+#"time.predictors.op =" permet d'indiquer la periode sur laquelle on synthetisera
+#"dependent ="  permet d'indiquer quelle variable sera synthetisee
+#"unit.variable=" permet d'indiquer le nom de la variable numérique qui sert a identifier les series
+#"unit.names.variable=" permet d'indiquer le nom de la variable alpha qui sert a identifier les series
+#"time.variable=" permet d'indiquer le nom de la variable temporelle
+#"treatment.identifier=" permet d'indiquer la serie que l'on voudra synthetiser
+#"controls.identifier=" permet d'indiquer les series de notre pool donateur
+#"time.optimize.ssr=" permet d'indiquer la periode sur laquelle les coeff de ponderations sont calcules
+#"time.plot =" permet de definir la periode sur laquelle les resultats sont indiques
+
+View(fra)
+
+#Affiche les differentes matrices construites en suivant les commandes entrees precedemment
 # PARTIE 2 #
 
 #Creation du vecteur de ponderations de la contribution des pays donateurs 
@@ -96,10 +115,14 @@ fra <- dataprep(foo = db,
 synth.fra <- synth(data.prep.obj = fra,
                            method = "BFGS")
 
-#Axe d'amelioration :
+#Pi : Il est possible d'utiliser plusieurs algos d'optimisation c("Nelder-Mead', 'BFGS', 'CG', 'L-BFGS-B', 'nlm', 'nlminb', 'spg', and 'ucminf")
 
-#(i) faire tourner avec d'autres variables predictors, pour constater la potentielle
-#amelioration de l'estimation (Hyp: elle sera forcemment plus precise car elle est 
-#au moins aussi precise que l'estimation qui considere seulement la variable composite)
+#Le BFGS est le quasi-Newton algorithm
 
-stargazer(synth.fra$solution.w)
+#Axe d'amelioration : Verifier le fonctionnement des differents algos, les tester si cela parait pertinent
+
+
+
+
+
+  
