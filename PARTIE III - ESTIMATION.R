6612fd4959c6d213a7edf7332d83646643b3ed05
Paul Favier <paul.favier4@gmail.com>
2022-12-11 00:26:35 +0100

    add estimation code

---
+++ PARTIE III - ESTIMATION.R
@@ -0,0 +1,12 @@
+#Code pour estimer le PIB#
+
+library(ggthemes)
+library(ggplot2)
+library(readr)
+library(dplyr)
+library(kableExtra)
+library(stargazer)
+library(readxl)
+library(Synth)
+library(base)
+library(utilities)
